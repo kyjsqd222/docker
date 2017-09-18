6fc9b9be1d6678e77ba13e43a49a6b99c66b2967
Administrator <812330932@qq.com>
2017-09-18 10:56:07 +0800

    modify for pep8

--- dockerauto.py
+++ dockerauto.py
@@ -6,13 +6,15 @@ import docker
 import os
 
 client = docker.DockerClient()
+
+
 # save all docker images on the server
 def save_images(dire=None):
     if not dire:
         dire = "/home/my_images/"
     try:
         os.makedirs(dire)
-    except OSError :
+    except OSError, e:
         pass
     ima_list = []
     for ima in client.images.list():
@@ -21,18 +23,19 @@ def save_images(dire=None):
         tar_name = im.encode("utf-8").split("/")[-1].split(":")[0]
         image = client.images.get(im)
         resp = client.images.model.save(image)
-        f = open('%s%s.tar'%(dire,tar_name),'w')
+        f = open('%s%s.tar' % (dire, tar_name), 'w')
         for chunk in resp.stream():
             f.write(chunk)
         f.close()
         break
 
+
 # load all docker images on the directions
 def load_images(dire=None):
     if not dire:
-        dire="./"
+        dire = "./"
     if not dire.endswith('/'):
-        dire+="/"
+        dire += "/"
     tar_list = os.listdir(dire)
     for im in tar_list:
         im=dire+im
@@ -40,12 +43,12 @@ def load_images(dire=None):
         with open(im) as f:
             client.images.load(f)
 
+
 # delete all images on the server
 def delete_images():
     for ima in client.images.list():
         ima_id = ima.short_id.encode('utf-8').split(':')[-1]
         client.images.remove(ima_id)
-
 
 
 if __name__ == '__main__':
